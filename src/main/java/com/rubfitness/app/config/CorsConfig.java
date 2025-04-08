40e8e7ca31cbec2c73dcf7fc61472bef71ac6e82
AURO RUBENS SILVA NETO <auro.rsneto@senacsp.edu.br>
2025-04-08 16:03:42 -0300

    Configurando novo endpoint no cors

--- src/main/java/com/rubfitness/app/config/CorsConfig.java
+++ src/main/java/com/rubfitness/app/config/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://frontend-rubfitnes.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
